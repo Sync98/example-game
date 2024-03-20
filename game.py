e58a4c945bcd57eb0c1418cbfcd422ba60273703
Karl <karlrobertkytt@gmail.com>
2024-03-20 13:45:54 +0200

    Add player and it's logic

--- game.py
+++ game.py
@@ -1,12 +1,21 @@
 import pygame
 from settings import settings
+from player import Player
+import game_functions as gf
 
 def run_game():
     pygame.init()
     gm_settings = settings()
-#shows screen1
+
     screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
     pygame.display.set_caption(gm_settings.caption)
+    
+    player = Player(screen)
+    
+    while True:
+        gf.check_events(player)
+        player.update()
+        gf.update_screen(gm_settings, screen, player)
 
 
     running = True
@@ -15,7 +24,13 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                
+        gf.check_events()
+        
+        player.blit_me()
+        
         pygame.display.flip()
+        
     pygame.quit()
 
 run_game()
