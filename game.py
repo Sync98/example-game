f476f404f07c9a9fcc11e6e6f2fb2cf10b57059f
Karl <karlrobertkytt@gmail.com>
2024-03-27 13:42:09 +0200

    As a player I can see Play button and game score at screen [Delivers #187313620]

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 import pygame
 from settings import settings
+from button import Button
 from player import Player
 from bubble import Bubble
+from scoreboard import Scoreboard
+from game_stats import GameStats
 import game_functions as gf
-#test1
+
 def run_game():
     pygame.init()
     gm_settings = settings()
@@ -11,16 +14,22 @@ def run_game():
     screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
     pygame.display.set_caption(gm_settings.caption)
     
+    play_button = Button(gm_settings, screen, "Play")
+    stats = GameStats()
+    sb = Scoreboard(gm_settings, screen, stats)
     clock = pygame.time.Clock()
     player = Player(screen)
     bubbles = pygame.sprite.Group()
    
     
     while True:
-        gf.check_events(gm_settings, screen, player, bubbles)
-        player.update()
-        gf.update_bubbles(player, bubbles)
-        bubbles.update()
-        gf.update_screen(gm_settings, screen, player, bubbles, clock)
+        gf.check_events(gm_settings, screen, player, bubbles, stats, play_button, sb)
+        if stats.game_active:
+            player.update()
+            gf.update_bubbles(player, bubbles, stats, sb)
+            bubbles.update()
+        else:
+            bubbles.empty()
+        gf.update_screen(gm_settings, screen, player, bubbles, clock, stats, play_button, sb)
 
 run_game()
